53a005a217e90f8ce6a3fa41308fb146200b6e9f
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-12-22 23:15:59 +0530

    First Class

---
+++ Ajay/22Dec/Code1.java
@@ -0,0 +1,8 @@
+//Basic Output of java
+
+public class Assignment{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
