a2255729c6cbd7ff58d8b13ed0fd592a7dea300b
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-12-23 22:53:38 +0530

    Array

---
+++ Ajay/23Dec/Code1.java
@@ -0,0 +1,22 @@
+
+public class Assignment{
+    public static void main(String[] args) {
+        //declaration of Array
+        int[] marks;
+        //Instantiate
+        marks = new int[5];
+        marks[0] = 5;
+        marks[1] = 3;
+        marks[2] = 4;
+
+        System.out.println(marks[2]);
+
+        //Other way
+        int[] price = new int[3];
+
+        //Other way
+        int[] numbers = {10,20,30,40,50};
+
+        System.out.println(numbers[3]);
+    }
+}
--- Ajay/23Dec/Notes.md
+++ Ajay/23Dec/Notes.md
@@ -1,3 +1,19 @@
 # ARRAYS 
 
 ![Array Visualization](./Images/Img1.png)
+
+** Declaration Of the array 
+
+Syntax for array declaration 
+
+            dataType[] arrayName; (*** Code1)
+
+Instantiation of this array:
+
+            arrayName = new dataType[size]; (***Code1)
+
+Ways to make the array (***Code1)
+
+
+
+
