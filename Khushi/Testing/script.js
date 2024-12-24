54fa0031f8d1e027216c8d7ebf6b1846bd4865fd
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-12-24 18:15:10 +0530

    update

---
+++ Khushi/Testing/script.js
@@ -0,0 +1,19 @@
+getName()
+
+console.log(x);
+
+function getName(){
+    console.log("MY name is Pranav");
+    console.log(y);
+    var y = 3;
+    
+}
+x2();
+
+var x2 = () =>{
+    console.log("Hello");
+}
+x2();
+var x = 5;
+
+console.log(x);
