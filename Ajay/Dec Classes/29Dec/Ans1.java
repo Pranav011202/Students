893ffef59d044480ce767363a7baf96eecb73c4e
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-12-31 20:40:38 +0530

    31dec

---
+++ Ajay/Dec Classes/29Dec/Ans1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Solution{
+
+    void rotateArrayByOne(int[] nums , int k){
+        for(int j=0 ; j<k ; j++){
+            int temp = nums[0];
+            for(int i=1 ; i<nums.length ; i++){
+                nums[i-1] = nums[i];
+            }
+
+            nums[nums.length-1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        //object for scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the value of k");
+        int k = sc.nextInt();
+
+        //declare the array
+        int[] nums = new int[n];
+
+        //populate the array
+        for(int i=0 ; i< nums.length ; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        Solution ajay = new Solution();
+
+        ajay.rotateArrayByOne(nums,k);
+
+        System.out.println("The rotated array is ");
+
+        for(int i :nums){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Ann/31Dec/notes.md
@@ -0,0 +1,12 @@
+# Ques Paper Discussion 
+
+** Ans 1) for , while and if are control flow statements while switch is a selection statement 
+** Ans 2) Time delay loop is a null/ empty loop which has no body 
+
+            for(int i=0 ; i<1000000 ; i++){
+
+            }
+
+** Ans 3) ![Ans 3][Ans3.png]
+
+        
